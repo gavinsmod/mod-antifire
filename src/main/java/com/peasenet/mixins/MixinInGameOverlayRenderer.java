487f645d109f6d6e67c2ba10e9357ff41443c7eb
Gavin <gavinpease@gmail.com>
2023-07-14 15:33:01 -0600

    Add mixin

--- src/main/java/com/peasenet/mixins/MixinInGameOverlayRenderer.java
+++ src/main/java/com/peasenet/mixins/MixinInGameOverlayRenderer.java
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2022. Gavin Pease and contributors.
+ * Copyright (c) 2022-2023. Gavin Pease and contributors.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction, including
  * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
  * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
  * following conditions:
+ *
  * The above copyright notice and this permission notice shall be included in all copies or substantial
  * portions of the Software.
  *
@@ -17,23 +18,25 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package com.peasenet.mod;
+package com.peasenet.mixins;
 
-import com.peasenet.main.GavinsMod;
-import com.peasenet.mods.render.ModAntiFire;
-import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.peasenet.util.event.EventManager;
+import com.peasenet.util.event.RenderOverlaySubmergedEvent;
+import com.peasenet.util.event.data.RenderSubmergedOverlay;
+import net.minecraft.client.gui.hud.InGameOverlayRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-public class AntiFire implements ModInitializer {
-    public static Logger LOGGER = LoggerFactory.getLogger("gavinsmod");
+@Mixin(InGameOverlayRenderer.class)
+public class MixinInGameOverlayRenderer {
 
-    /**
-     * Runs the mod initializer.
-     */
-    @Override
-    public void onInitialize() {
-        LOGGER.info("Hello Fabric world!");
-        GavinsMod.addMod(new ModAntiFire());
+    @Inject(at = @At("HEAD"), method = "renderFireOverlay", cancellable = true)
+    private static void onRenderFireOverlay(CallbackInfo ci) {
+        var evt = new RenderOverlaySubmergedEvent(new RenderSubmergedOverlay(RenderSubmergedOverlay.SubmergedOverlayType.FIRE));
+        EventManager.getEventManager().call(evt);
+        if (evt.isCancelled())
+            ci.cancel();
     }
 }
